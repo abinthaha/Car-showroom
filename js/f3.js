be6a466dd447b45de7c4bb9bd0be7cce3aaf7da5
ess_ess <sravan@qburst.com>
2015-07-28 17:12:27 +0530

    Finised , whatever >):(

--- js/f3.js
+++ js/f3.js
@@ -2,7 +2,7 @@ function setVideoSize(){
     var h, w;
     h = $(window).height();
     w = $(window).width();
-    
+    $('.full-page-block').css("height",h);
 }
 
 
@@ -10,4 +10,17 @@ function setVideoSize(){
 $(document).ready(function () {
    setVideoSize();
     $(window).resize(setVideoSize());
+    $("#pull-out-menu").hide();
+    $("#drop-down-menu").click(function(){
+        $("#pull-out-menu").slideDown();
+    });
+    $("#pull-out-menu").click(function(){
+         $("#pull-out-menu").slideUp('slow');
+    });
+    $(".menu-button").click(function(){
+        var todiv;
+        todiv = "#"+$(this).data("id");
+        $('html,body').animate({ scrollTop: $(todiv).offset().top }, 'slow');
+       // $(window).scrollTop($(todiv).offset().top);
+    });
 });
